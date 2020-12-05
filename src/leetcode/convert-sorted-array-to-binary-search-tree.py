0279fc0798b2b3173043c1f322890285f805fc5d
Victor Ponamarev <vponamarev@guess.com>
2020-12-05 13:05:44 +0300

    leetcode: convert-sorted-array-to-binary-search-tree

--- src/leetcode/convert-sorted-array-to-binary-search-tree.py
+++ src/leetcode/convert-sorted-array-to-binary-search-tree.py
@@ -1,9 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         m = len(nums) // 2
@@ -12,17 +14,25 @@ class Solution:
         self.bin_assign(r, 0, m, len(nums), nums)
         
         return r
-    
+
     def bin_assign(self, root, i, m, j, nums: List[int]):
         l = (i + m) // 2
-        
+
         if l != m:
-            print(l)
             root.left = TreeNode(nums[l])
-            self.bin_assign(root.left, l, i, m, nums)
-        
+            self.bin_assign(root.left, i, l, m, nums)
+
         r = (j + m) // 2
-        
+
         if r != m:
             root.right = TreeNode(nums[r])
             self.bin_assign(root.right, m + 1, r, j, nums)
+
+fin = open('input.txt')
+
+s = Solution()
+arr = [int(i) for i in fin.readline().split(',')]
+print(arr)
+s.sortedArrayToBST(arr)
+
+fin.close()
