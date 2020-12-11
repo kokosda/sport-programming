e20adf8ce14790e96b8dfd71383af2b4bb0e2fcf
Victor Ponamarev <vponamarev@guess.com>
2020-12-11 19:07:48 +0300

    leetcode: conclusion_942_buildTree: recursive approach.

--- src/leetcode/conclusion_942_buildTree.py
+++ src/leetcode/conclusion_942_buildTree.py
@@ -8,28 +8,55 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         if inorder == None or postorder == None:
             return None
-        
+
         if len(inorder) == 0 or len(postorder) == 0:
             return None
-        
+
         root = TreeNode(postorder[-1])
         root_ind = inorder.index(root.val)
 
-        def go_down(inorder, postorder, root, flag):
+        def go_down(inorder, postorder):
+            print("inorder ", inorder, " posorder", postorder)
             if len(inorder) == 0:
-                return
+                return None
 
             if len(inorder) == 1:
-                if flag == 0:
-                    root.left = TreeNode(inorder[0])
-                else:
-                    root.right = TreeNode(inorder[0])
+                return TreeNode(inorder[0])
 
-                return
-
+            root = TreeNode(postorder[-1])
             root_ind = inorder.index(root.val)
-        
-        go_down(inorder[0:root_ind], postorder[0:root_ind], root, 0)
-        go_down(inorder[root_ind + 1:], postorder[root_ind:-1], root, 1)
-        
+
+            root.left = go_down(inorder[0:root_ind], postorder[0:root_ind])
+            root.right = go_down(inorder[root_ind + 1:], postorder[root_ind:-1])
+            
+            return root
+
+        root.left = go_down(inorder[0:root_ind], postorder[0:root_ind])
+        root.right = go_down(inorder[root_ind + 1:], postorder[root_ind:-1])
+
         return root
+"""
+inorder: [9,5,3,2,19,8,0,1]
+postord: [9,2,19,3,5,1,0,8]
+
+inorder_l1: [9,5,3,2,19] root 8
+postord_l1: [9,2,19,3,5] root 8
+
+inorder_l2: [9] root 5
+postord_l2: [9] root 5
+inorder_r2: [3,2,19] root 5
+postord_r2: [2,19,3] root 5
+
+inorder_l3: [] root 3
+postord_l3: [] root 3
+inorder_r3: [2,19] root 3
+postord_r3: [2,19] root 3
+
+inorder_l4: [2] (root 19)
+postord_l4: [2] (root 19)
+inorder_r4: [] root 19
+postord_r4: [] root 19
+
+inorder_r1: [0,1] root 8
+postord_r1: [1,0] root 8
+"""
