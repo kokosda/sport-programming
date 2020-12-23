90384552a3d61cf6f8eefe16928ad7de0260338a
Victor Ponamarev <vponamarev@guess.com>
2020-12-24 01:01:42 +0300

    leetcode: basic_operations_in_a_bst_1006_deleteNode (2)

--- src/leetcode/basic_operations_in_a_bst_1006_deleteNode.py
+++ src/leetcode/basic_operations_in_a_bst_1006_deleteNode.py
@@ -9,71 +9,57 @@ class Solution:
         if not root:
             return None
         
-        node, parent = self.find_node(root, key)
+        node, node_parent = self.find_node(root, key)
         
         if not node:
             return root
         
         if not node.left and not node.right:
-            self.delete_source_node(node, parent, None, None)
-            
-            if node == root:
-                return None
+            self.replace_parent_successor_with_next_node(node, node_parent, None)
+        elif node.left and not node.right:
+            self.replace_parent_successor_with_next_node(node, node_parent, node.left)
+        elif node.right and not node.left:
+            self.replace_parent_successor_with_next_node(node, node_parent, node.right)
         else:
-            replacement_node, replacement_parent = self.get_replacement(node, parent)
-            self.delete_source_node(node, parent, replacement_node, replacement_parent)
-
+            inorder_successor, inorder_successor_parent = self.get_inorder_successor(node.right, node)
+            self.swap_nodes(node, node_parent, inorder_successor, inorder_successor_parent)
+            self.replace_parent_successor_with_next_node(node, inorder_successor_parent, None)
+        
         return root
-    
-    def find_node(self, root: TreeNode, key: int) -> TreeNode:
-        node = root;
+        
+    def find_node(self, root: TreeNode, val: int) -> TreeNode:
+        node = root
         parent = None
         
-        while node and node.val != key:
-            if node.val > key:
-                parent = node
+        while node and node.val != val:
+            parent = node
+            
+            if val < node.val:
                 node = node.left
             else:
-                parent = node
                 node = node.right
                 
         return [node, parent]
     
-    def get_replacement(self, sourceNode: TreeNode, sourceParent: TreeNode) -> TreeNode:
-        node = sourceNode
-        parent = sourceParent
+    def get_inorder_successor(self, root: TreeNode, parent: TreeNode) -> TreeNode:
+        node = root
         
-        if not node.right:
-            return [node.left, parent]
-        
-        parent = node
-        node = node.right
-        
-        while True:
-            if node.left:
-                parent = node
-                node = node.left
-            else:
-                break
-
-        return [node, parent]
-            
-    def delete_source_node(self, sourceNode: TreeNode, sourceParent: TreeNode, targetNode: TreeNode, targetParent: TreeNode):
-        if sourceParent:
-            if sourceParent.left == sourceNode:
-                sourceParent.left = targetNode
-            else:
-                sourceParent.right = targetNode
-            
-        if targetNode:
-            if targetNode != sourceNode.left:
-                targetNode.left = sourceNode.left
+        while node.left:
+            parent = node
+            node = node.left
                 
-            if targetNode != sourceNode.right:
-                targetNode.right = sourceNode.right
-            
-        if targetParent:
-            if targetParent.left == targetNode:
-                targetParent.left = None
-            else:
-                targetParent.right = None
+        return [node, parent]
+    
+    def swap_nodes(self, source: TreeNode, sourceParent: TreeNode, target: TreeNode, targetParent: TreeNode):
+        self.replace_parent_successor_with_next_node(source, sourceParent, target)
+        self.replace_parent_successor_with_next_node(target, targetParent, source)
+        
+        tmp_target_left, tmp_target_right = target.left, target.right
+        target.left, target.right = source.left, source.right
+        source.left, source.right = tmp_target_left, tmp_target_right
+    
+    def replace_parent_successor_with_next_node(self, node: TreeNode, parent: TreeNode, next_node: TreeNode):
+        if parent.left == node:
+            parent.left = next_node
+        else:
+            parent.right = next_node
