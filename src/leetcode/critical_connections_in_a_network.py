ad5c51c8d58efbdb106bd4f0249d881a0b5e5368
Victor Ponamarev <vponamrev@gmail.com>
2020-12-29 12:48:22 +0300

    leetcode: critical_connections_in_a_network (3)

--- src/leetcode/critical_connections_in_a_network.py
+++ src/leetcode/critical_connections_in_a_network.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         ht = self.__convert_to_hash_table(n, connections)
@@ -8,6 +6,7 @@ class Solution:
         for edge in connections:
             used = [0] * n
             used[0] = 1
+            used_assignments = 1
             v0, v = 0, None
             queue, qi = [v0], 0
             
@@ -19,12 +18,11 @@ class Solution:
                         if not self.__is_vu_in_edge(v, u, edge):
                             queue.append(u)
                             used[u] = 1
+                            used_assignments += 1
                     
                 qi += 1
-            
-            unused = list(filter(lambda x: (x == 0), used))
-            
-            if len(unused) > 0:
+                
+            if used_assignments < n:
                 res.append(edge)
 
         return res
@@ -46,8 +44,6 @@ class Solution:
     def __is_vu_in_edge(self, v: int, u: int, edge: List[int]) -> bool:
         return (edge[0] == v or edge[1] == v) and (edge[0] == u or edge[1] == u)
     
-s = Solution()
-s.criticalConnections(1, [])
 """
 {0: {1,2}}
 {1: {0,2,3}}
