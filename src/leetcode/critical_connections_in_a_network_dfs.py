d867ff134a7a6d625e2f7eb35dd23c93e31590d9
Victor Ponamarev <vponamrev@gmail.com>
2021-01-02 02:08:21 +0300

    leetcode: critical_connection_in_a_network_bfs and dfs

---
+++ src/leetcode/critical_connections_in_a_network_dfs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        
+    
+"""
+{0: {1,2}}
+{1: {0,2,3}}
+{2: {0,1,5}}
+{3: {1,4}}
+{4: {3}}
+{5: {2}}
+
+6
+[[0,1],[0,2],[1,2],[1,3],[2,5],[3,4]]
+"""
