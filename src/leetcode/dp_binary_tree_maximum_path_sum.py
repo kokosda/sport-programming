3fc21e6471e9a2998c6a798c5f4db2cebb8418b8
Victor Ponamarev <vponamrev@gmail.com>
2021-01-08 12:21:59 +0300

    leetcode: dp_binary_tree_maximum_path_sum

--- src/leetcode/dp_binary_tree_maximum_path_sum.py
+++ src/leetcode/dp_binary_tree_maximum_path_sum.py
@@ -5,32 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxPathSum(self, root: TreeNode) -> int:        
-        def dfs(node: TreeNode) -> int:
+    def maxPathSum(self, root: TreeNode) -> int:
+        dp = dict()
+        """
+        {
+            {root} : 1,
+            {left} : 2,
+            {right} : 3
+            ...
+        }
+        """
+
+        def dfs(node: TreeNode, max_s: int) -> int:
             if not node:
-                return 0
+                return max_s
             
             if not node.left and not node.right:
-                return node.val
+                return max(node.val, max_s)
             
-            left_val = dfs(node.left)
-            right_val = dfs(node.right)
+            max_s = max(dfs(node.left, max_s), dfs(node.right, max_s))
+            left_val = node.left.val if node.left else 0
+            right_val = node.right.val if node.right else 0
+            max_s = max(max_s, node.val + left_val + right_val, node.val, left_val, right_val)
+            node.val = max(node.val + left_val, node.val + right_val)
             print(f'n={node.val} l={left_val}, r={right_val}')
-            
-            max_s = max(node.val + left_val, node.val + right_val, node.val + left_val + right_val, node.val, left_val, right_val)
             
             return max_s
                 
-        res = dfs(root)
+        res = dfs(root, root.val)
         return res
+    
 """
-state: current_val
-value func: max sum
-transition funcs: 
+dynamic state: node (value)
+dynamic value func: path of max sum
+dynamic initial states: leaf values
+dynamic transition funcs: 
     1) node_i.val + l.val + r.val
     2) node_i.val + l.val
     3) node_i.val + r.val
     4) node_i.val
-order: dfs
-answer:
+order of calculation: dfs
+answer to the original problem: 
 """
