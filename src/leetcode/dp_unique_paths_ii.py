ded1a82af248ab4df0de983c63fe2e12489194c2
Victor Ponamarev <vponamrev@gmail.com>
2021-01-22 12:12:46 +0300

    leetcode: dp_unique_paths and dp_unique_paths_ii

--- src/leetcode/dp_unique_paths_ii.py
+++ src/leetcode/dp_unique_paths_ii.py
@@ -6,30 +6,22 @@ class Solution:
         dp = [[0] * (len(obstacleGrid[0])) for i in range(len(obstacleGrid))]
         dp[0][0] = 1
 
-        for i in range(len(obstacleGrid)):
-            for j in range(len(obstacleGrid[i])):
-                if (i + 1 == len(obstacleGrid)) and (j + 1 == len(obstacleGrid[i])):
-                    if i > 0:
-                        dp[i][j] += dp[i - 1][j]
-                    if j > 0:
-                        dp[i][j] += dp[i][j - 1]
+        for j in range(1, len(obstacleGrid[0])):
+            if obstacleGrid[0][j] == 1:
+                break
+                
+            dp[0][j] = 1
+                
+        for i in range(1, len(obstacleGrid)):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = dp[i - 1][0]
+                
+            for j in range(1, len(obstacleGrid[i])):
+                if obstacleGrid[i][j] == 1:
                     continue
-                if i + 1 == len(obstacleGrid) or j + 1 == len(obstacleGrid[i]):
-                    continue
-                if obstacleGrid[i + 1][j] == 0:
-                    dp[i + 1][j] += dp[i][j]
                     
-                    if i > 0:
-                        dp[i + 1][j] += dp[i - 1][j]
-                if obstacleGrid[i][j + 1] == 0:
-                    dp[i][j + 1] += dp[i][j]
-                    
-                    if j > 0:
-                        dp[i][j + 1] += dp[i][j - 1]
-                        
-        for row in dp:
-            print(row)
-            pass
+                dp[i][j] = dp[i-1][j] + dp[i][j - 1]
+                
         return dp[-1][-1]
     
 """
