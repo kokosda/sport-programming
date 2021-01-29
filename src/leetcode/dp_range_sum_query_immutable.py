5a4bd5853397e247a9a34cf9186e397e51877d20
Victor Ponamarev <vponamrev@gmail.com>
2021-01-29 10:42:10 +0300

    leetcode: dp_range_sum_query_immutable

--- src/leetcode/dp_range_sum_query_immutable.py
+++ src/leetcode/dp_range_sum_query_immutable.py
@@ -2,35 +2,35 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.__nums__ = nums
-        self.__dp__ = [[0] * len(nums) for _ in range(len(nums))]
-        self.__precalculate_sums()
+        self.__dp__ = [0] * (len(nums) + 1)
+        self.__precalc__()
 
     def sumRange(self, i: int, j: int) -> int:
-        res = self.__dp__[i][j]
+        res = self.__dp__[j + 1] - self.__dp__[i]
         return res
-    
-    def __precalculate_sums(self):
-        dp = self.__dp__
-        nums = self.__nums__
         
-        for i in range(len(nums)):
-            dp[i][i] = nums[i]
-            
-            for j in range(i + 1, len(nums)):
-                dp[i][j] = nums[j] + dp[i][j - 1]
-                
-        #for dpi in dp:
-         #   print(*dpi)
-        #print()
+    def __precalc__(self):
+        nums = self.__nums__
+        dp = self.__dp__
+        
+        for i, _ in enumerate(nums):
+            dp[i + 1] = dp[i] + nums[i]
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(i,j)
 
 """
-input: [2,5,0,-1,4,6,1,3]
-dp: [2, 7, 7, 6]
-(2;4): 0 + -1 + 4 = 3
-(3;5): -1 + 4 + 6 = 9
-
+["NumArray","sumRange","sumRange","sumRange","sumRange"]
+[[[2,5,1,-1,4,6,1,3]],[2,4],[3,5],[0,5],[1,1]]
+["NumArray","sumRange","sumRange","sumRange","sumRange"]
+[[[2,5,0,-1,4,6,1,3]],[2,4],[3,5],[0,5],[1,1]]
+["NumArray","sumRange","sumRange","sumRange"]
+[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]
+["NumArray"]
+[[[]]]
+["NumArray","sumRange"]
+[[[1]],[0,0]]
+["NumArray","sumRange"]
+[[[2,5]],[0,0]]
 """
