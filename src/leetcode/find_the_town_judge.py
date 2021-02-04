a68b5cf320d1382c44169283131c1cc020732ca9
Victor Ponamarev <vponamrev@gmail.com>
2021-02-04 10:26:38 +0300

    leetcode: find_the_town_judge (more optimal)

--- src/leetcode/find_the_town_judge.py
+++ src/leetcode/find_the_town_judge.py
@@ -5,6 +5,30 @@ class Solution:
         if not len(trust):
             return -1
         
+        d = [0] * (N + 1)
+        p = [0] * (N + 1)
+        max_c, judge = 0, -1
+        
+        for pair in trust:
+            i, j = pair
+            d[j] += 1
+            p[i] += 1
+            
+            if d[j] > max_c:
+                max_c = d[j]
+                judge = j
+        
+        if judge > -1 and d[judge] == (N - 1) and p[judge] == 0:
+            return judge
+        
+        return -1
+    
+    def findJudgeMatrix(self, N: int, trust: List[List[int]]) -> int:
+        if N == 1:
+            return 1
+        if not len(trust):
+            return -1
+        
         m = [[0] * (N + 1) for _ in range(N + 1)]
         
         for pair in trust:
@@ -32,5 +56,6 @@ class Solution:
         return judge
     
 """
-
+d = [0,0,0,0,N-1,0]
+p = [0,0,0,0,0,0]
 """
