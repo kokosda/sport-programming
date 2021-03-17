16db7d8e2e4b53b21845e6d5982d68cadaef8ffe
Victor Ponamarev <vponamarev@guess.com>
2021-03-17 11:30:01 +0300

    leetcode: 8458_G_interesnoe_puteshestive

--- src/yandex/8458_G_interesnoe_puteshestive.py
+++ src/yandex/8458_G_interesnoe_puteshestive.py
@@ -9,14 +9,20 @@ for i in range(v):
 		m[i][j] = abs(pair[i][0] - pair[j][0]) + abs(pair[i][1] - pair[j][1])
 		m[j][i] = m[i][j]
 
-queue = [v1]
-used = [False] * v
-used[v1] = True
-
 if m[v1][v2] <= k:
 	print(1)
 else:
+	bfs(v1, m)
+		
+def bfs(vs, m, k):
+	queue = queue.Queue()
+	queue.put(vs)
+	visited = [False] * v
+	visited[vs] = True
 	min_count = 0
 
 	while queue:
-		vx = queue.pop()
+		vx = queue.get()
+
+		for i in m[vs]:
+			if m
