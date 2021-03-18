9771aa471b9401689b8e3b2946739c2bbc62157c
Victor Ponamarev <vponamarev@guess.com>
2021-03-18 21:06:23 +0300

    leetcode: k_th_largest_element_in_a_stream

---
+++ src/leetcode/k_th_largest_element_in_a_stream.py
@@ -0,0 +1,37 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self._k = k
+        self._nums = sorted(nums)[-k:]
+        self._k_pos = 0
+
+    def add(self, val: int) -> int:
+        i = bisect.bisect_left(self._nums, val)
+        
+        if len(self._nums) < self._k:
+            self._nums = (self._nums[:i] + [val] + self._nums[i:])[-self._k:]
+            return self._nums[0]
+
+        if i > 0:
+            if self._nums[-1] <= val:
+                self._nums.append(val)
+                self._k_pos += 1
+            else:
+                self._nums = (self._nums[:i] + [val] + self._nums[i:])[-self._k:]
+                self._k_pos = 0
+                
+        return self._nums[self._k_pos]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+"""
+["KthLargest","add","add","add","add","add"]
+[[2,[0]],[-1],[1],[-2],[-4],[3]]
+["KthLargest","add","add","add","add","add"]
+[[1,[]],[-3],[-2],[-4],[0],[4]]
+["KthLargest","add","add","add","add","add"]
+[[3,[4,5,8,2]],[3],[5],[10],[9],[4]]
+"""
