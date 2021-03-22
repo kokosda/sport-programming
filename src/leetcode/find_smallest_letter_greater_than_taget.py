db3cb2d5f323cab3d6fcda2372e9aee308d689bf
Victor Ponamarev <vponamarev@guess.com>
2021-03-22 15:20:27 +0300

    leetcode: find_smallest_letter_greater_than_taget

--- src/leetcode/find_smallest_letter_greater_than_taget.py
+++ src/leetcode/find_smallest_letter_greater_than_taget.py
@@ -1,5 +1,19 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        s = 0
+        e = len(letters)
+        
+        while s < e:
+            m = (s + e) // 2
+            
+            if letters[m] <= target:
+                s = m + 1
+            else:
+                e = m
+
+        return letters[s] if s < len(letters) else letters[0]
+    
+    def nextGreatestLetter_set(self, letters: List[str], target: str) -> str:
         store = [0] * 26
         a_code = ord('a')
         unique = set(letters)
