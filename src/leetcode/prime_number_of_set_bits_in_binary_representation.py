7048ebbf9ef5dff86986c2a293c5dd1570ecbad4
Victor Ponamarev <vponamarev@guess.com>
2021-03-23 14:36:38 +0300

    leetcode: prime_number_of_set_bits_in_binary_representation (2)

--- src/leetcode/prime_number_of_set_bits_in_binary_representation.py
+++ src/leetcode/prime_number_of_set_bits_in_binary_representation.py
@@ -1,5 +1,33 @@
 class Solution:
     def countPrimeSetBits(self, L: int, R: int) -> int:
+        primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 }
+        res = 0
+        
+        for i in range(L, R + 1):
+            n = i
+            bits = 0
+            
+            while n > 0:
+                bits += n & 1
+                n >>= 1
+                
+            if bits in primes:
+                res += 1
+        
+        return res
+    
+    def _get_primes() -> set[int]:
+        res = { i for i in range(2, 32) }
+        
+        for i in range(2, 32):
+            for j in range(2, i):
+                if i != j and i % j is 0 and i in res:
+                    res.remove(i)
+                    
+        return res
+                    
+    
+    def countPrimeSetBits_remainder(self, L: int, R: int) -> int:
         ranges = [bin(i).count('1') for i in range(L, R + 1)]
         res = 0
         
@@ -18,3 +46,9 @@ class Solution:
                 return False
             
         return True
+    
+"""
+010011001
+001001100
+000000001
+"""
