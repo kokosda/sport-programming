18df06ddc506f312307646bf373f5761483d336d
Victor Ponamarev <vponamarev@guess.com>
2021-03-24 23:15:56 +0300

    leetcode: minimum_number_of_vertices_to_reach_all_nodes (2)

--- src/leetcode/minimum_number_of_vertices_to_reach_all_nodes.py
+++ src/leetcode/minimum_number_of_vertices_to_reach_all_nodes.py
@@ -1,5 +1,15 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        store = set([i for i in range(n)])
+        
+        for e in edges:
+            if e[1] in store:
+                store.remove(e[1])
+            
+        res = list(store)
+        return res
+    
+    def findSmallestSetOfVertices_list(self, n: int, edges: List[List[int]]) -> List[int]:
         store = [i for i in range(n)]
         
         for e in edges:
