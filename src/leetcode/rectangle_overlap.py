b68380a7a66899c8aa02292568f26cea6706a77d
Victor Ponamarev <vponamarev@guess.com>
2021-03-25 19:49:17 +0300

    leetcode: rectangle_overlap

---
+++ src/leetcode/rectangle_overlap.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if rec1[0] == rec1[2] or rec1[1] == rec1[3] or \
+            rec2[0] == rec2[2] or rec2[1] == rec2[3]:
+            return False
+        
+        res = rec1[2] > rec2[0] and rec1[3] > rec2[1] and \
+            rec1[0] < rec2[2] and rec1[1] < rec2[3]
+        return res
