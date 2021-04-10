634c51cdc7536fdede3b1b5ad070077df9742144
Victor Ponamarev <vponamarev@guess.com>
2021-04-10 12:45:36 +0300

    leetcode: long_pressed_name

---
+++ src/leetcode/long_pressed_name.py
@@ -0,0 +1,62 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        if len(typed) < len(name):
+            return False
+        
+        i = 0
+        j = 0
+        prev_i = 0
+        
+        while i < len(name):
+            if name[i] == typed[j]:
+                prev_i = i
+                i += 1
+                j += 1
+            else:
+                prev_j = j
+                
+                while j < len(typed) and name[prev_i] == typed[j]:
+                    j += 1
+                else:
+                    if prev_j is j or name[i] != typed[j]:
+                        return False
+                    
+        while j < len(typed):
+            if typed[j] != name[-1]:
+                return False
+            j += 1
+            
+        return i == len(name) and j == len(typed)
+"""
+n:aalex
+      ^
+t:aaaleex
+       ^
+     
+saeed
+   ^
+ssaaedd
+     ^
+
+cn: 1
+ct: 2
+
+"rick"
+"kric"
+"alex"
+"aaleex"
+"saeed"
+"ssaaedd"
+"leelee"
+"lleeelee"
+"t"
+"m"
+"tt"
+"t"
+"tt"
+"ttt"
+"tt"
+"tt"
+
+вспомнить, что сделали в i_odd, i_even (просто взяли 2 указатели и двигали когда пора и смотрели на их состояние)
+"""
