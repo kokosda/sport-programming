c306eddfb016fbfe05a49acb4181ac838e70c5e7
Victor Ponamarev <vponamarev@guess.com>
2021-04-10 17:15:28 +0300

    leetcode: delete_columns_to_make_sorted

---
+++ src/leetcode/delete_columns_to_make_sorted.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        n = len(strs)
+        m = len(strs[0])
+        res = 0
+        
+        for i in range(m):
+            for j in range(1, n):
+                if strs[j][i] < strs[j - 1][i]:
+                    res += 1
+                    break
+                    
+        return res
+                
+"""
+"abcd"
+"efgh"
+"wxyz"
+"""
