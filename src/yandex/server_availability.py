32854b7486e19f5e72cc58bc91154cfad80d35e3
Victor Ponamarev <vponamarev@guess.com>
2021-05-31 23:35:01 +0300

    yandex: server_availability

--- src/yandex/server_availability.py
+++ src/yandex/server_availability.py
@@ -1,14 +1,92 @@
+from collections import deque
+from typing import Deque, Tuple
+
+class Request:
+    def __init__(self, time: int, count: int) -> None:
+        self.time = time
+        self.count = count
+
+    def __str__(self) -> str:
+        return f'Request({self.time}, {self.count})'
+
+    def __repr__(self) -> str:
+        return f'Request({self.time}, {self.count})'
+
+def collapse_queue(q: Deque[Request], timestamp: int, duration: int) -> int:
+    if len(q) is 0:
+        return
+
+    diff = 0
+
+    if q[-1].time + duration < timestamp:
+        q.clear()
+        return
+
+    while len(q) is not 0 and q[-1].time - q[0].time > duration:
+        request = q.pop()
+        diff = request.count - diff
+
+        if len(q) is not 0:
+            q[-1].count -= diff
+
+def is_request_allowed(q: Deque[Request], limit: int) -> bool:
+    if len(q) is not 0 and q[-1].count == limit:
+        return False
+
+    return True
+
+def add_request(q: Deque[Request], timestamp: int) -> None:
+    if len(q) is 0:
+        q.appendleft(Request(timestamp, 1))
+        return
+
+    if q[-1].time == timestamp:
+        q[-1].count += 1
+    else: 
+        q.appendleft(Request(timestamp, q[-1].count + 1))
+
 fin = open('input.txt')
 user_limit, service_limit, duration = map(int, fin.readline().split())
-s = fin.readline().split()
+s = [None]
+requests_by_users = {}
+requests_by_service = deque()
 
-while s[0] != '-1':
-    user_id, timestamp = map(int, s)
+while True:
     s = fin.readline().split()
+
+    if s[0] == '-1':
+        break
+
+    timestamp, user_id = map(int, s)
+
+    collapse_queue(requests_by_service, timestamp, duration)
+
+    if not is_request_allowed(requests_by_service, service_limit):
+        print(500, requests_by_service)
+        continue
+
+    if requests_by_users.get(user_id) is None:
+        requests_by_users[user_id] = deque()
+
+    user_q = requests_by_users[user_id]
+
+    collapse_queue(user_q, timestamp, duration)
+
+    if not is_request_allowed(user_q, user_limit):
+        print(400, requests_by_users)
+        continue
+
+    add_request(user_q, timestamp)
+    add_request(requests_by_service, timestamp)
+    print(200, user_q)
 
 fin.close()
 
 """
+мы храним запросы на пользователя в словаре: 
+    <user_id>: queue[(time, requests_count)]
+также нужен счётчик сервисных запросов за последние n сек.
+
 2(userlim) 5(serv) 5(dur)
 time, user_id
 1 100 -- 200
@@ -51,4 +129,10 @@ users:
         3: err2
     }
 }
+user_limit = 5, duration = 5
+
+[[1,3]]
+[[2,4]]
+[[3,5]]
+[[8,4]]
 """
